b5491f0a307860c198f200424b84fab13955e2ae
Roderic Bos <github@rooc.nl>
2026-01-22 17:14:52 +0100

    allow for arrays

--- Pages/JsonEdit.cshtml.cs
+++ Pages/JsonEdit.cshtml.cs
@@ -5,98 +5,155 @@ using System.Text.Json;
 
 namespace QuickJsonEdit.Pages
 {
-public class JsonEditModel : PageModel
-{
-    public void CollectEditablePaths(JsonNode node, string prefix, List<string> paths)
+    public class JsonEditModel : PageModel
     {
-        if (node is JsonObject obj)
+    public void CollectEditablePaths(JsonNode node, string prefix, List<string> paths)
+{
+    if (node is JsonObject obj)
+    {
+        foreach (var kvp in obj)
         {
-            foreach (var kvp in obj)
+            var key = kvp.Key;
+            var child = kvp.Value;
+            string currentPath = string.IsNullOrEmpty(prefix) ? key : prefix + "/" + key;
+            if (child is JsonValue val)
             {
-                var key = kvp.Key;
-                var child = kvp.Value;
-                string currentPath = string.IsNullOrEmpty(prefix) ? key : prefix + "/" + key;
-                if (child is JsonValue val)
+                if (val.GetValueKind() == JsonValueKind.String)
                 {
-                    if (val.GetValueKind() == JsonValueKind.String)
+                    paths.Add(currentPath);
+                }
+            }
+            else if (child is JsonObject)
+            {
+                // recurse
+                CollectEditablePaths(child, currentPath, paths);
+            }
+            else if (child is JsonArray arr)
+            {
+                for (int i = 0; i < arr.Count; i++)
+                {
+                    var elem = arr[i];
+                    string arrPath = currentPath + "/[" + i + "]";
+                    if (elem is JsonValue v && v.GetValueKind() == JsonValueKind.String)
                     {
-                        paths.Add(currentPath);
+                        paths.Add(arrPath);
+                    }
+                    else
+                    {
+                        // recurse into array element
+                        CollectEditablePaths(elem, arrPath, paths);
                     }
                 }
-                else if (child is JsonObject)
-                {
-                    // recurse
-                    CollectEditablePaths(child, currentPath, paths);
-                }
-                // arrays are skipped
             }
         }
     }
-
-    public JsonNode? GetNodeAtPath(string path)
+    else if (node is JsonArray arr)
     {
-        var parts = path.Split('/');
-        JsonNode? currentNode = Config.JsonDocument;
-        foreach (var part in parts)
+        for (int i = 0; i < arr.Count; i++)
         {
-            if (currentNode is JsonObject obj)
+            var elem = arr[i];
+            string arrPath = string.IsNullOrEmpty(prefix) ? "[" + i + "]" : prefix + "/[" + i + "]";
+            if (elem is JsonValue v && v.GetValueKind() == JsonValueKind.String)
             {
-                if (obj.ContainsKey(part))
-                {
-                    currentNode = obj[part];
-                }
-                else
-                {
-                    return null;
-                }
+                paths.Add(arrPath);
+            }
+            else
+            {
+                CollectEditablePaths(elem, arrPath, paths);
+            }
+        }
+    }
+}
+
+
+
+public JsonNode? GetNodeAtPath(string path)
+{
+    var parts = SplitPath(path);
+    JsonNode? currentNode = Config.JsonDocument;
+    foreach (var part in parts)
+    {
+        if (string.IsNullOrEmpty(part)) continue;
+        if (currentNode is JsonObject obj)
+        {
+            if (obj.ContainsKey(part))
+            {
+                currentNode = obj[part];
             }
             else
             {
                 return null;
             }
         }
-        return currentNode;
-    }
-
-    public bool TryUpdateValueAtPath(string path, string newValue)
-    {
-        var parts = path.Split('/');
-        JsonNode? currentNode = Config.JsonDocument;
-        for(int i=0; i < parts.Length - 1; i++)
+        else if (currentNode is JsonArray arr)
         {
-            var part = parts[i];
-            if (currentNode is JsonObject obj)
+            if (TryParseArrayIndex(part, out int index) && index >= 0 && index < arr.Count)
             {
-                if (obj.ContainsKey(part))
-                {
-                    currentNode = obj[part];
-                }
-                else
-                {
-                    // Create new JsonObject if missing intermediate key
-                    var newObj = new JsonObject();
-                    obj[part] = newObj;
-                    currentNode = newObj;
-                }
+                currentNode = arr[index];
             }
             else
             {
-                return false;
+                return null;
             }
         }
-        // Update last part
-        string lastPart = parts[^1];
-        if (currentNode is JsonObject parentObj)
+        else
         {
-            // overwrite or create new string value
-            parentObj[lastPart] = newValue;
-            return true;
+            return null;
         }
-        return false;
     }
+    return currentNode;
+}
+
+private string[] SplitPath(string path)
+{
+    var parts = path.Split('/', StringSplitOptions.RemoveEmptyEntries);
+    var processedParts = new List<string>();
+    foreach (var part in parts)
+    {
+        if (part.Contains('['))
+        {
+            int start = 0;
+            for (int i = 0; i < part.Length; i++)
+            {
+                if (part[i] == '[')
+                {
+                    if (i > start)
+                        processedParts.Add(part.Substring(start, i - start));
+                    int end = part.IndexOf(']', i);
+                    if (end == -1)
+                        processedParts.Add(part.Substring(i));
+                    else
+                        processedParts.Add(part.Substring(i, end - i + 1));
+                    start = end + 1;
+                    i = end;
+                }
+            }
+            if (start < part.Length)
+                processedParts.Add(part.Substring(start));
+        }
+        else
+        {
+            processedParts.Add(part);
+        }
+    }
+    return processedParts.ToArray();
+}
+
+private bool TryParseArrayIndex(string segment, out int index)
+{
+    index = -1;
+    if (segment.StartsWith("[") && segment.EndsWith("]"))
+    {
+        var numberStr = segment.Substring(1, segment.Length - 2);
+        return int.TryParse(numberStr, out index);
+    }
+    return false;
+}
+
+    
 
 
-        [BindProperty(SupportsGet = true)]
+[BindProperty(SupportsGet = true)]
         public string? Key { get; set; }
         public JsonConfig Config { get; }
         public JsonEditModel(JsonConfig config)
@@ -107,6 +164,73 @@ public class JsonEditModel : PageModel
         public void OnGet()
         {
         }
+
+    public bool TryUpdateValueAtPath(string path, string newValue)
+    {
+        var parts = SplitPath(path);
+        JsonNode? currentNode = Config.JsonDocument;
+        for (int i = 0; i < parts.Length - 1; i++)
+        {
+            var part = parts[i];
+            if (string.IsNullOrEmpty(part)) continue;
+            if (currentNode is JsonObject obj)
+            {
+                if (obj.ContainsKey(part))
+                {
+                    currentNode = obj[part];
+                }
+                else
+                {
+                    // Create new JsonObject if missing key
+                    var newObj = new JsonObject();
+                    obj[part] = newObj;
+                    currentNode = newObj;
+                }
+            }
+            else if (currentNode is JsonArray arr)
+            {
+                if (TryParseArrayIndex(part, out int index))
+                {
+                    if (index >= 0 && index < arr.Count)
+                    {
+                        currentNode = arr[index];
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        string lastPart = parts[^1];
+        if (string.IsNullOrEmpty(lastPart)) return false;
+        if (currentNode is JsonObject parentObj)
+        {
+            parentObj[lastPart] = newValue;
+            return true;
+        }
+        else if (currentNode is JsonArray parentArr)
+        {
+            if (TryParseArrayIndex(lastPart, out int index))
+            {
+                if (index >= 0 && index < parentArr.Count)
+                {
+                    parentArr[index] = newValue;
+                    return true;
+                }
+            }
+            return false;
+        }
+        return false;
+    }
 
 public IActionResult OnPostSaveNewValue()
         {
@@ -121,6 +245,8 @@ public IActionResult OnPostSaveNewValue()
             }
             return RedirectToPage(new { Key = k, Saved = true });
         }
+
+    
 
         public IActionResult OnPostSaveFile()
         {
